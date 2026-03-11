4b786e0a315a664da3c0ed19a127fcf99b5551cb
Shrey Dhiman <39773350+theshreydhiman@users.noreply.github.com>
2026-03-11 23:45:19 +0530

    fix(#1): setState inside forEach overwrites state on each iteration instead of accumulating results
    Modify the setState call to accumulate results in an array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Movie from "./searchInput";
-import React from "react";
-import "./App.css";
-import axios from "axios";
-import Moviereview from "./components/movieReview";
+
+import Movie from './searchInput';
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import Moviereview from './components/movieReview';
 
 class App extends React.Component {
   state = {
@@ -15,18 +16,20 @@ class App extends React.Component {
     );
     // console.log(response.data);
 
-    response.data.Search.forEach(async (elemnet) => {
+    const results = await Promise.all(response.data.Search.map(async (element) => {
       const Response = await axios.get(
-        `https://www.omdbapi.com/?i=${elemnet.imdbID}&apikey=21193e36`
+        `https://www.omdbapi.com/?i=${element.imdbID}&apikey=21193e36`
       );
+      return Response.data;
+    }));
 
-      this.setState({ Rivew: Response.data });
-      console.log(this.state.Rivew);
-    });
+    this.setState({ Rivew: results });
+    console.log(this.state.Rivew);
   };
+
   render() {
     return (
-      <div className="ui container" style={{ marginTop: "30px" }}>
+      <div className='ui container' style={{ marginTop: '30px' }}>
         <Movie onSearchSubmit={this.onSearchSubmit} />
         {/* <Response temp={this.state.temp} /> */}
         <Moviereview Rivew={this.state.Rivew} />
